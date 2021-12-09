595b1f1b741786a46555d2894da043a3fde1aa58
macbook <bludohric@gmail.com>
2021-12-09 14:05:17 +0000

    prepare v1.1

--- connected.py
+++ connected.py
@@ -5,13 +5,29 @@ from kivy.properties import StringProperty
 import os
 import requests
 
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+
+from kivy.storage.jsonstore import JsonStore
+
+import serial
+import serial.tools.list_ports
+
+
 class MyStack(StackLayout):
     pass
 
 
 class Connected(Screen):
+    dialog = None
+    store = JsonStore('auth.json')
+    port_value = ""
     def disconnect(self):
         self.manager.transition = SlideTransition(direction="right")
+        if self.store.exists('user'):
+            self.store.delete('user')
         self.manager.current = 'login'
         self.manager.get_screen('login').resetForm()
 
@@ -28,9 +44,85 @@ class Connected(Screen):
             'destination_code': destination_code,
             'carrier_code': carrier_code,
             'transport_number': transport_number,
-            'weight': weight
+            'weight': weight,
+            'userid': self.store.get('user')['userid']
         }
-        url = 'http://localhost:8000/api/v1/submit-weight'
+        for key in dataReq:
+            if dataReq[key] and dataReq[key].strip():
+                pass
+            else:
+                self.onPopPress("Missing field: " + key)
+                return
+
+        url = 'https://seyclock.com/api/v1/submit-weight'
         response = requests.post(url, data = dataReq)
         resData = response.json()['success']
-        print(resData['passed'])
+
+        print(resData)
+
+        if resData['passed'] == 1:
+            self.onPopPress("Data successfully sent!")
+            self.resetForm()
+        else:
+            self.onPopPress("Error processing data!")
+            self.resetForm()
+
+
+    def onPopPress(self, message):
+          
+        layout = GridLayout(cols = 1, padding = 5)
+  
+        popupLabel = Label(text = message)
+        closeButton = Button(text = "Close")
+  
+        layout.add_widget(popupLabel)
+        layout.add_widget(closeButton)       
+  
+        # Instantiate the modal popup and display
+        popup = Popup(title ='Alert',
+                      content = layout,
+                      size_hint =(None, None), size =(400, 400))  
+        popup.open()   
+  
+        # Attach close button press with popup.dismiss action
+        closeButton.bind(on_press = popup.dismiss)
+
+
+    def resetForm(self):
+        self.ids.uld_number.text = ""
+        self.ids.station_code.text = ""
+        self.ids.destination_code.text = ""
+        self.ids.carrier_code.text = ""
+        self.ids.transport_number.text = ""
+        self.ids.weight.text = ""
+
+    def readWeight(self):
+        port_number = self.ids.port_number.text
+        ports = serial.tools.list_ports.comports()
+        self.port_value = ""
+        port_list = []
+
+        if(len(port_list) < 1):
+            self.onPopPress("No ports opened or available!")
+            return
+
+        for sel_port in ports:
+            port_list.append(str(sel_port))
+
+        for x in range(0, len(port_list)):
+            if port_list[x].startswith(port_number):
+                self.port_value = port_number
+        
+        if self.port_value and self.port_value.strip():
+            pass
+        else:
+            self.onPopPress("Port Selected not available!")
+            return
+        
+
+        serialBout = serial.Serial()
+        serialBout.port = port_number
+        serialBout.open()
+
+        packet = serialBout.readline()
+        self.ids.weight.text = packet.decode("utf").rstrip("\n")
--- main.py
+++ main.py
@@ -4,11 +4,20 @@ from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
 import os
 from kivy.core.window import Window
 
+from kivy.storage.jsonstore import JsonStore
+
 from connected import Connected
+import requests
+
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
 
 Window.clearcolor = (1,1,1,1)
 
 class Login(Screen):
+    store = JsonStore('auth.json')
     def do_login(self, loginText, passwordText):
         app = App.get_running_app()
 
@@ -16,7 +25,36 @@ class Login(Screen):
         app.password = passwordText
 
         self.manager.transition = SlideTransition(direction="left")
-        self.manager.current = 'connected'
+        # self.manager.current = 'connected'
+
+        dataReq = {
+            'email': self.ids.login.text,
+            'password': self.ids.password.text,
+        }
+
+        for key in dataReq:
+            if dataReq[key] and dataReq[key].strip():
+                pass
+            else:
+                self.onPopPress("Missing field: " + key)
+                return
+
+        url = 'https://seyclock.com/api/v1/login'
+        response = requests.post(url, data = dataReq)
+        resData = response.json()['success']
+
+        print(resData)
+
+        if resData['passed'] == 1:
+            self.store.put('user',
+                name=resData['user']['name'], email=resData['user']['email'],
+                gender=resData['user']['gender'], userid=resData['user']['userid']
+            )
+            self.resetForm()
+            self.manager.current = 'connected'
+        else:
+            self.onPopPress(resData['message'])
+            self.resetForm()
 
         app.config.read(app.get_application_config())
         app.config.write()
@@ -24,6 +62,25 @@ class Login(Screen):
     def resetForm(self):
         self.ids['login'].text = ""
         self.ids['password'].text = ""
+
+    def onPopPress(self, message):
+          
+        layout = GridLayout(cols = 1, padding = 5)
+  
+        popupLabel = Label(text = message)
+        closeButton = Button(text = "Close")
+  
+        layout.add_widget(popupLabel)
+        layout.add_widget(closeButton)       
+  
+        # Instantiate the modal popup and display
+        popup = Popup(title ='Alert',
+                      content = layout,
+                      size_hint =(None, None), size =(400, 400))  
+        popup.open()   
+  
+        # Attach close button press with popup.dismiss action
+        closeButton.bind(on_press = popup.dismiss)
 
 class LoginApp(App):
     username = StringProperty(None)
